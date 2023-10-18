6bddab29dc1dcebdffca950b73a4aa23a4bdd93e
Moe <mohamedmidani2001@gmail.com>
2023-10-17 22:21:10 -0400

    Initial Commit

---
+++ Serah.V1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Serah.V1
+//
+//  Created by Mohamed Midani on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Serah.V1/Serah_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Serah_V1App.swift
+//  Serah.V1
+//
+//  Created by Mohamed Midani on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Serah_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
