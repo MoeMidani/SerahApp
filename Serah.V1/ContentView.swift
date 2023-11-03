c381c4e1e02d1f465c92cb3529743dfc5aeaa0ed
Moe <mohamedmidani2001@gmail.com>
2023-11-03 09:49:42 -0400

    updates

--- Serah.V1/ContentView.swift
+++ Serah.V1/ContentView.swift
@@ -18,17 +18,17 @@ struct TriviaQuestion {
 
 
 struct ContentView: View {
-//    func fetchQuestion(_ difficult: Int)->String{
-////        questionPulled.toggle()
-//        return "Hello"
-//    }
+    //    func fetchQuestion(_ difficult: Int)->String{
+    ////        questionPulled.toggle()
+    //        return "Hello"
+    //    }
     
     //    @State private var isQuestionInProgress = false
     @State private var isShowing = false
     @State private var text = "..."
     @State private var difficultyLevel : Int = 1
     @State private var questionPulled : Bool = false
-//    @State var questionATM : String = ""
+    //    @State var questionATM : String = ""
     @State var boxView : Bool = false
     //    let timer : Int
     
@@ -36,57 +36,57 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-//            if boxView{difficultyLevel)
-//                boxQuestionView(diffcultcy: 0)
-//            }else{
-                VStack {
-                    VStack{
-                        Image("logo")
-                        Spacer()
-                        ZStack{
-                            Rectangle()
-                                .fill(CustomColor.boxColor)
-                                .frame(width: 340, height: 210)
-                                .cornerRadius(50)
-                                .overlay( RoundedRectangle(cornerRadius: 50)
-                                    .stroke(Color.white, lineWidth:6)
-                                )
-                                .shadow(radius: 7)
-                            
-                            Text("السلام عليكم ورحمة الله وبركاته").font(.title2)
-                                .fontWeight(.semibold)
-                                .offset(y: -70)
-                                .padding(.bottom, 10)
-                            Text("اختر المستوى التالي و أجب على الأسئلة ضمن الوقت المحدود").font(.headline.italic())
-                                .fontWeight(.regular)
-                                .multilineTextAlignment(.center)
-                                .padding(.horizontal, 69.0)
-                        }
-//                        Spacer()
-//                        Spacer()
-//                        Spacer()
-//                        Spacer()
-                        Spacer(minLength: 350)
-
-}
-//                    boxQuestionView(question: "HI", timer: 0)
+            if (boxView){
+              boxQuestionView(difficulty: 1)
+                        }else{
+            VStack {
+                VStack{
+                    Image("logo")
+                    Spacer()
                     ZStack{
                         Rectangle()
-                            .fill(CustomColor.smallBox)
-                            .frame(width: 320, height: 90)
-                        // .blur(radius: 3)
+                            .fill(CustomColor.boxColor)
+                            .frame(width: 340, height: 210)
                             .cornerRadius(50)
                             .overlay( RoundedRectangle(cornerRadius: 50)
-                                .stroke(Color.black, lineWidth:6))
-                            .shadow(radius: 10)
+                                .stroke(Color.white, lineWidth:6)
+                            )
+                            .shadow(radius: 7)
                         
-                        VStack{
-                            if (questionPulled){
-                            Button("Start"){
-                                questionPulled.toggle()
-                                boxView.toggle()
-                                //                            boxQuestionView(question: questionATM, timer: 1)
+                        Text("السلام عليكم ورحمة الله وبركاته").font(.title2)
+                            .fontWeight(.semibold)
+                            .offset(y: -70)
+                            .padding(.bottom, 10)
+                        Text("اختر المستوى التالي و أجب على الأسئلة ضمن الوقت المحدود").font(.headline.italic())
+                            .fontWeight(.regular)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 69.0)
+                    }
+                   
+                    Spacer(minLength: 350)
+                    
+                }
+                
+                ZStack{
+                    Rectangle()
+                        .fill(CustomColor.smallBox)
+                        .frame(width: 320, height: 90)
+                        .cornerRadius(50)
+                        .overlay( RoundedRectangle(cornerRadius: 50)
+                            .stroke(Color.black, lineWidth:6))
+                        .shadow(radius: 10)
+                    
+                    VStack{
+                        if (questionPulled){
+                            HStack{
+                                Button("ابدأ"){
+                                    questionPulled.toggle()
+                                    boxView.toggle()}.foregroundColor(.black)
                                 
+                                    Button("back"){
+                                        questionPulled.toggle()
+                                        
+                                    }
                             }
                         }else{
                             Button("المستوى"){
@@ -99,22 +99,25 @@ struct ContentView: View {
                                 
                                 Button("مبتدأ"){
                                     difficultyLevel = 0
-                                 questionPulled.toggle()
-//                                    questionATM = fetchQuestion(0)
+                                    questionPulled.toggle()
+                                    //                                    questionATM = fetchQuestion(0)
                                     // question = "Hello"
                                     // Timer = 1
                                     // isQuestionInProgress = true
-                                    
                                     
                                 }
                                 
                                 Button("متوسط"){
                                     difficultyLevel = 1
+                                    questionPulled.toggle()
+
                                     
                                 }
                                 
                                 Button("خبير"){
                                     difficultyLevel = 2
+                                    questionPulled.toggle()
+
                                     
                                 }
                                 
@@ -122,14 +125,14 @@ struct ContentView: View {
                                 
                             }
                         }}}
-                    Spacer()
-                }
-                
+                Spacer()
             }
             
-            
-        }}
-//}
+        }
+        
+        
+    }}
+}
 
 
 
